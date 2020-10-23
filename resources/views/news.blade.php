fe89a0bf639dc029b13b6471380b0d49bbabe9a1
GIanni Ruggiero <gianni@groovecompany.it>
2020-10-23 17:54:22 +0200

    view prodotti costruita in modo dinamico con doppio foreach

--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -8,7 +8,6 @@
     <div class="wrap-news">
         <div class="container">
             <h1>News dal mondo La Molisana</h1>
-            <p><i class="fas fa-angle-right"></i></p>
         </div>
     </div>
 @endsection
--- resources/views/prodotti.blade.php
+++ resources/views/prodotti.blade.php
@@ -6,108 +6,40 @@
 
 
 @section('mainContent')
+
+{{-- Wrapper pagina Prodotti --}}
 <div class="wrap-prodotti">
+    {{-- Continaer --}}
     <div class="container">
-        {{-- sezione LE LUNGHE --}}
-        @if (!empty($paste["lunga"]))
-            <h2>LE LUNGHE</h2>
+    {{-- Prodotti divisi per tipo di pasta --}}
+    @foreach ($paste as $key => $pasta)
+        @if (!empty($paste[$key]))
+            <h2>{{$key}}</h2>
             <div class="tipo-pasta">
-            @foreach ($paste["lunga"] as $prodotto)
-                <div class="box">
-                {{-- immagine del tipo di pasta  --}}
-                <img class="pic" src="{{ $prodotto['src'] }}" alt="">
-                {{-- livello overlay --}}
-                <div class="overlay">
-                    {{-- wrapper titolo pasta e icona ristorante --}}
-                    <div class="wrap-hover">
-                    {{-- imposta la URL per il link alla pagina prodotto specifica --}}
-                    <a href="{{route('prodotto-singolo', $prodotto['id'])}}"><h3>{{ $prodotto['titolo'] }}</h3></a>
-                    <a href="{{route('prodotto-singolo', $prodotto['id'])}}"><img class="ico-rist" src="{{asset('images/ico-pasta.png')}}" alt="icona"></a>
+                @foreach ($paste[$key] as $prodotto)
+                    <div class="box">
+                        {{-- immagine del tipo di pasta  --}}
+                        <img class="pic" src="{{ $prodotto['src'] }}" alt="">
+                        {{-- livello overlay --}}
+                        <div class="overlay">
+                            {{-- wrapper titolo pasta e icona ristorante --}}
+                            <div class="wrap-hover">
+                            {{-- imposta la URL per il link alla pagina prodotto specifica --}}
+                            <a href="{{route('prodotto-singolo', $prodotto['id'])}}"><h3>{{ $prodotto['titolo'] }}</h3></a>
+                            <a href="{{route('prodotto-singolo', $prodotto['id'])}}"><img class="ico-rist" src="{{asset('images/ico-pasta.png')}}" alt="icona"></a>
+                            </div>
+                            {{-- /wrapper titolo pasta e icona ristorante --}}
+                        </div>  
+                        {{-- /livello overlay --}}
                     </div>
-                    {{-- /wrapper titolo pasta e icona ristorante --}}
-                </div>  
-                {{-- /livello overlay --}}
-                
-                </div>
-            @endforeach
-            </div>
-        @endif
-
-        {{-- sezione LE CORTE --}}
-        @if (!empty($paste["corta"]))
-            <h2>LE CORTE</h2>
-            <div class="tipo-pasta">
-            @foreach ($paste["corta"] as $prodotto)
-
-                <div class="box">
-                {{-- immagine del tipo di pasta  --}}
-                <img class="pic" src="{{ $prodotto['src'] }}" alt="">
-                {{-- livello overlay --}}
-                <div class="overlay">
-                    {{-- wrapper titolo pasta e icona ristorante --}}
-                    <div class="wrap-hover">
-                    {{-- imposta la URL per il link alla pagina prodotto specifica --}}
-                    <a href="prodotti/show/{{ $prodotto['id'] }}"><h3>{{ $prodotto['titolo'] }}</h3></a>
-                    <a href="prodotti/show/{{ $prodotto['id'] }}"><img class="ico-rist" src="{{asset('images/ico-pasta.png')}}" alt="icona"></a>
-                    </div>
-                    {{-- /wrapper titolo pasta e icona ristorante --}}
-                </div>  
-                {{-- /livello overlay --}}
-                </div>
-            
                 @endforeach
-            </div>
+            </div>    
         @endif
-
-        {{-- sezione LE CORTISSIME --}}
-        @if (!empty($paste["cortissima"]))
-            <h2>LE CORTISSIME</h2>
-            <div class="tipo-pasta">
-            @foreach ($paste["cortissima"] as $prodotto)
-                <div class="box">
-                {{-- immagine del tipo di pasta  --}}
-                <img class="pic" src="{{ $prodotto['src'] }}" alt="">
-                {{-- livello overlay --}}
-                <div class="overlay">
-                    {{-- wrapper titolo pasta e icona ristorante --}}
-                    <div class="wrap-hover">
-                    {{-- imposta la URL per il link alla pagina prodotto specifica --}}
-                    <a href="prodotti/show/{{ $prodotto['id'] }}"><h3>{{ $prodotto['titolo'] }}</h3></a>
-                    <a href="prodotti/show/{{ $prodotto['id'] }}"><img class="ico-rist" src="{{asset('images/ico-pasta.png')}}" alt="icona"></a>
-                    </div>
-                    {{-- /wrapper titolo pasta e icona ristorante --}}
-                </div>  
-                {{-- /livello overlay --}}
-                </div>
-            @endforeach
-            </div>
-        @endif
+    @endforeach
     </div>
+    {{-- /Continaer --}}
 </div>
-  
-@endsection
-
-
-{{-- @dd($paste["lunga"]); --}}
-
-
-
-
-
-
-{{-- 
-
-@section('mainContent')
-@if (!empty($paste))
-  @foreach ($paste as $key => $tipopasta)
-    <h2>$key</h2>
-    <ul>
-      <p>$tipopasta</p>
-    </ul>
-   @endforeach
-@endif  
+{{-- /Wrapper pagina Prodotti --}}
 
 @endsection
 
-
-@dd($paste); --}}
